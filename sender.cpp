16807159079554392430b396e6102af2fbedfd91
Phoebe Chang <natsuki011077@ucla.edu>
2014-11-22 14:57:34 -0800

    update sender

--- sender.cpp
+++ sender.cpp
@@ -15,5 +15,39 @@ void error(char *msg)
 
 int main (int argc, char* argv[])
 {
+  // Make sure the port number is provided.   
+  if (argc < 2) {
+    fprintf(stderr,"USAGE: %s PORT\n", argv[0]);
+    exit(1);
+  }
+
+  // Obtain the fd for UDP listening socket.
+  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sockfd < 0) {
+    error("ERROR: sender open socket failure\n");
+  }
+  printf("Sender socket created\n");
+
+
+  int port = atoi(argv[1]);
+  struct sockaddr_in sender_addr;
+  
+  memset((char*) &sender_addr, 0, sizeof(sender_addr)) ;
+  sender_addr.sin_family = AF_INET;
+  sender_addr.sin_addr.s_addr = INADDR_ANY;
+  sender_addr.sin_port = htons(port);
+
+  if (bind(sockfd, (struct sockaddr *) &sender_addr, sizeof(sender_addr)) < 0) {
+    close(sockfd);
+    error("ERROR: sender bind socket failure\n");
+  }
+  printf("server bind to port %d, done\n", sockfd);
+
+  // Listen to receiver
+  while (1) {
+    
+  }
+  
+
   return 0;
 }
