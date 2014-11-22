1824a86388c6d3791ead50033c55f0c476f19959
natsuki011077 <natsuki011077@ucla.edu>
2014-11-22 12:17:10 -0800

    Update sender.cpp

--- sender.cpp
+++ sender.cpp
@@ -1,1 +1,19 @@
-#include
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+void error(char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main (int argc, char* argv[])
+{
+  return 0;
+}
